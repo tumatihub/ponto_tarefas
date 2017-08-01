0dfbff244a7286b9c4b89dcbc8f9901228eba555
Tumati <jgsoaresdias@gmail.com>
2017-07-31 23:05:12 -0300

    Login basico com um botao

--- login.php
+++ login.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 	<title>Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-
-<a href="validaLogin.php">Login</a>
+	<section class="tarefas-container">
+		<a class="btn btn-login" href="validaLogin.php">Login</a>
+	</section>
 </body>
 </html>
